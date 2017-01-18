fc7208db144413df74f4e78297173a59942712f3
CarlosLopezP <isaac_clip@outlook.com>
2017-01-18 00:29:53 -0600

    n

--- AutomatizacionVentas/AutomatizacionVentas/Form1.Designer.cs
+++ AutomatizacionVentas/AutomatizacionVentas/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(322, 244);
+            this.ClientSize = new System.Drawing.Size(421, 246);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
